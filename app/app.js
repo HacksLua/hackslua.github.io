040916e0f99355eff1c502d32d4adaa544706107
HacksLua <parkyrist.ak@gmail.com>
2018-07-30 12:26:55 +0700

    qw
    qw

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var app = angular.module('eventsApp', ['angular-loading-bar', 'googleApi', 'tree
 
 app.config(function(googleLoginProvider) {
     googleLoginProvider.configure({
-        clientId: '271838702018-jcc58isp1cu2c9j35rpv6i57mbrtcu71.apps.googleusercontent.com',
+        clientId: '997074517462-7prl4d0ii90eiabr97ektqp7busu3igh.apps.googleusercontent.com',
         scopes: ["https://www.googleapis.com/auth/userinfo.email", 
         	     "https://www.googleapis.com/auth/calendar", 
         	     "https://www.googleapis.com/auth/plus.login"]
